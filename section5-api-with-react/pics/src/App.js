e405c16618e9ef9bf72c48b4e59a586318c4d09d
kirbeee <pspsean1234@gmail.com>
2024-07-31 11:26:35 +0800

    complete with unsplash api search image function

---
+++ section5-api-with-react/pics/src/App.js
@@ -0,0 +1,16 @@
+import SearchBar from "./components/SearchBar";
+import searchImages from "./api";
+
+function App() {
+    const handleSubmit = (term) => {
+        searchImages(term);
+    }
+
+    return (
+    <div>
+        <SearchBar onSubmit={handleSubmit} />
+    </div>
+  );
+}
+
+export default App;
