d89e3f320f3eb601f0f5aea1a58b1288e30da264
kirbeee <pspsean1234@gmail.com>
2024-08-03 15:30:22 +0800

    complete with book create part

--- section6-Handle-forms/books/src/components/BookCreate.js
+++ section6-Handle-forms/books/src/components/BookCreate.js
@@ -1,6 +1,25 @@
-function BookCreate(){
+import { useState } from 'react';
+
+function BookCreate({ onCreate }) {
+  const [title, setTitle] = useState('');
+
+  const handleChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        onCreate(title);
+        setTitle('');
+    }
+
     return (
         <div>
+            <form onSubmit={handleSubmit}>
+                <label>Title</label>
+                <input value={title} onChange={handleChange} type="text" />
+                <button >Create</button>
+            </form>
             <h1>BookCreate</h1>
         </div>
     )
