3ab26aadde947178171c44f51cb1293a8b830490
kirbeee <pspsean1234@gmail.com>
2024-08-08 10:49:26 +0800

    start with tailwind and create button component

--- section10-routing-system/comps/src/App.js
+++ section10-routing-system/comps/src/App.js
@@ -1,6 +1,24 @@
+import {GoBell} from "react-icons/go";
+import Button from "./Button";
+
 function App(){
     return (
         <div>
+            <div>
+                <Button primary rounded outline><GoBell className="mr-1"/>Testing</Button>
+            </div>
+            <div>
+                <Button secondary>Testing</Button>
+            </div>
+            <div>
+                <Button success>Testing</Button>
+            </div>
+            <div>
+                <Button warning>Testing</Button>
+            </div>
+            <div>
+                <Button danger>Testing</Button>
+            </div>
             <h1>App</h1>
         </div>
     )
