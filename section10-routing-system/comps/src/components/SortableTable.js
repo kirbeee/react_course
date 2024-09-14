941897a88d5764f34aa32f76ba5192b0c95ff667
kirbeee <pspsean1234@gmail.com>
2024-09-14 17:39:11 +0800

    add sorting function

--- section10-routing-system/comps/src/components/SortableTable.js
+++ section10-routing-system/comps/src/components/SortableTable.js
@@ -1,16 +1,57 @@
+import { useState } from "react";
 import Table from "./Table";
 
-function SortableTable(props) {
-    const { config } = props;
+function SortableTable({data ,config, keyFn }) {
+    const [sortOrder, setSortOrder] = useState(null);
+    const [sortBy, setSortBy] = useState(null);
+
+    const handleClick = (label) => {
+        switch (sortOrder) {
+            case 'asc':
+                setSortOrder('desc');
+                setSortBy(label);
+                break;
+            case 'desc':
+                setSortOrder(null);
+                setSortBy(null);
+                break;
+            case null:
+                setSortOrder('asc');
+                setSortBy(label);
+                break;
+        }
+
+    }
 
     const updatedConfig = config.map((column) => {
         return (column.sortValue) ? {
             ...column,
-            header: () => <th>{column.label} IS sortAble</th>
+            header: () => <th onClick={()=>handleClick(column.label)}>{column.label} IS sortAble</th>
         } : column
     })
 
-    return <Table {...props} config={updatedConfig}/>;
+    let sortedData = data;
+    if (sortOrder && sortBy) {
+        const {sortValue} = config.find((column) => column.label === sortBy)
+        sortedData = [...data].sort((a, b) => {
+            const valueA = sortValue(a);
+            const valueB = sortValue(b);
+
+            const reverseOrder = (sortOrder === 'asc') ? 1 : -1;
+
+            if (typeof valueA === 'string') {
+                return valueA.localeCompare(valueB) * reverseOrder;
+            }else{
+                return (valueA - valueB) * reverseOrder;
+            }
+        })
+    }
+
+    return (
+        <div>{sortOrder} - {sortBy}
+        <Table keyFn={keyFn} data={sortedData} config={updatedConfig}/>
+        </div>
+)
 }
 
 export default SortableTable;
