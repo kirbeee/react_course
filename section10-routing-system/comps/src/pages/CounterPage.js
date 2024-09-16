aabd902160bdc08bf3f93514cfdab23e55734bae
kirbeee <pspsean1234@gmail.com>
2024-09-16 13:34:18 +0800

    yesterday

--- section10-routing-system/comps/src/pages/CounterPage.js
+++ section10-routing-system/comps/src/pages/CounterPage.js
@@ -1,41 +1,76 @@
 import Button from "../components/Button";
-import {useState} from "react";
+import {useReducer} from "react";
 import Panel from "../components/Panel";
 
-function CounterPage( {initialCount = 0} ) {
-    const [count, setCount] = useState(initialCount)
-    const [valueToAdd, setValueToAdd] = useState(0);
-    const increment = () =>{
-        setCount(count + 1)
+const INCREMENT_COUNT = 'increment';
+const DECREMENT_COUNT = 'decrement';
+const SET_VALUE_TO_ADD = 'change-value-to-add';
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case INCREMENT_COUNT:
+            return {
+                ...state,
+                count: state.count + 1
+            }
+        case DECREMENT_COUNT:
+            return {
+                ...state,
+                count: state.count - 1
+            }
+        case SET_VALUE_TO_ADD:
+            return {
+                ...state,
+                valueToAdd: action.payload
+            }
+        default:
+            return state;
     }
-    const decrement = ()=>{
-        setCount(count - 1)
+}
+
+function CounterPage({initialCount = 0}) {
+    // const [count, setCount] = useState(initialCount)
+    // const [valueToAdd, setValueToAdd] = useState(0);
+    const [state, dispatch] = useReducer(reducer, {
+        count: initialCount,
+        valueToAdd: 0
+    })
+
+    const increment = () => {
+        dispatch({type: INCREMENT_COUNT});
+    }
+    const decrement = () => {
+        dispatch({type: DECREMENT_COUNT});
     }
 
-    const handleChange = (event) =>{
-        const value = parseInt(event.target.value)||0;
-        setValueToAdd(value);
+    const handleChange = (event) => {
+        const value = parseInt(event.target.value) || 0;
+        dispatch({
+            type: SET_VALUE_TO_ADD,
+            payload: value
+        });
         console.log(value);
     }
 
-    const handleSubmit = (event) =>{
+    const handleSubmit = (event) => {
         event.preventDefault();
-        setCount(count + valueToAdd);
-        setValueToAdd(0)
+
+        // setCount(count + valueToAdd);
+        // setValueToAdd(0)
     }
 
     return (
         <Panel className="m-3">
-            <h1 className="text-lg">Count is {count} </h1>
+            <h1 className="text-lg">Count is {state.count} </h1>
             <div className="flex flex-row">
-            <Button onClick={increment} >Increment</Button>
-            <Button onClick={decrement} >Increment</Button>
+                <Button onClick={increment}>Increment</Button>
+                <Button onClick={decrement}>Increment</Button>
             </div>
 
             <form onSubmit={handleSubmit}>
                 <label>Add a lot!</label>
                 <input
-                    value={valueToAdd || ""}
+                    value={state.valueToAdd || ""}
                     onChange={handleChange}
                     type="number"
                     className="p-1 m-3 bg-gray-300"/>
