045b5e8cd72cced2d7df20fcb2f858ca47064623
kirbeee <pspsean1234@gmail.com>
2024-09-25 19:06:47 +0800

    finish the delete user feature

--- section21-api-Async-Thunks/media/src/store/thunks/removeUser.js
+++ section21-api-Async-Thunks/media/src/store/thunks/removeUser.js
@@ -2,10 +2,8 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 const removeUser = createAsyncThunk('users/remove', async (user) => {
-    const response = await axios.delete(`http://localhost:3005/users/${user.id}`);
-
-    // TODO: fix the response data
-    return response.data;
+    await axios.delete(`http://localhost:3005/users/${user.id}`);
+    return user;
 })
 
 export { removeUser };
